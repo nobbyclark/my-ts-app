f9497533601b02e2cc07aeef29e5a03ffd6ee6c7
Nick Clark <nick.clark@dovetailstudios.com>
2022-02-09 13:34:01 +1300

    Update Greeting Loader to use msw

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,15 +11,8 @@ async function reportError(error: Error, errorInfo: ErrorInfo) {
   return { success: true };
 }
 
-const greetings = ["Hello", "Hi", "Hey there", `What's up`, "Howdy", `G'day`];
-
 async function loadGreeting(subject: string) {
-  return { data: { greeting: `${await fetchRandomGreeting()} ${subject}` } };
-}
-
-async function fetchRandomGreeting() {
-  await sleep(1000);
-  return greetings[Math.floor(Math.random() * greetings.length)];
+  return { data: { greeting: `Hello ${subject}` } };
 }
 
 export { reportError, loadGreeting };
--- src/components/greeting-loader.test.tsx
+++ src/components/greeting-loader.test.tsx
@@ -1,25 +1,32 @@
+import "whatwg-fetch";
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import { GreetingLoader } from "./greeting-loader";
-import { loadGreeting as mockLoadGreeting } from "../api/api";
 
-jest.mock("../api/api");
+interface Body {
+  subject: string;
+}
+
+const server = setupServer(
+  rest.post<Body>("/greeting", (req, res, ctx) => {
+    return res(ctx.json({ data: { greeting: `Hello ${req.body.subject}` } }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
 
 test("loads greeting on click", async () => {
-  (mockLoadGreeting as jest.Mock).mockResolvedValueOnce({
-    data: { greeting: "TEST_GREETING" },
-  });
   render(<GreetingLoader />);
   const nameInput = screen.getByLabelText(/name/i);
   const loadButton = screen.getByText(/load/i);
   userEvent.type(nameInput, "Nick");
   userEvent.click(loadButton);
-  expect(mockLoadGreeting).toHaveBeenCalledWith("Nick");
-  expect(mockLoadGreeting).toHaveBeenCalledTimes(1);
   await waitFor(() =>
-    expect(screen.getByLabelText(/greeting/i)).toHaveTextContent(
-      "TEST_GREETING"
-    )
+    expect(screen.getByLabelText(/greeting/i)).toHaveTextContent("Hello Nick")
   );
 });
